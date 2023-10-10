a6518b7926aa5a16b929bfcef0cd36ca97c3ff56
binhantt <doan44503@gmail.com>
2023-10-11 00:56:07 +0700

    vuex

--- src/store/modules/demo.js
+++ src/store/modules/demo.js
@@ -1,27 +1,34 @@
 
 // initial state
 const state = {
-   
+    song:{
+    name:'hocsib',
+    singer:'fwewewewe',
+    path:'../src/components/nhac/EmGiOi-ICMJackJ97KICM-9430870.mp3',
+    image:'../src/components/icon/war.jpg',
+}
+    
 };
 
 // getters lay
 const getters = {
   //  allTodos: (state) => state.todos,
-
+  song: (state) => state.song
 };
 
 // actions doi 
 const actions = {
     async fetchTodos({ commit }) {
-        
-    },
- 
-};
+    } ,
+    setsong({ commit }, newsong) {
+       commit("setsong", newsong) ;
+    }
+  };
 
 // commit=>mutations
 const mutations = {
     //setTodos: (state, todos) => state.todos = todos,
- 
+    setsong: (state, newsong) => state.song = newsong
 };
 
 export default {
--- src/views/home.vue
+++ src/views/home.vue
@@ -1,9 +1,45 @@
-
-<script  setup>
-import navar  from '../components/layout/navar.vue'
-import foder from '../components/layout/foder.vue';
-</script>
 <template>
-   <navar/>
-   <foder/>
+  <div>
+    <navar />
+    <foder />
+    <input ref="searchInput" type="text" v-model="searchQuery" @input="search" />
+    <button @click="setSongFiltered(song1)">Set Song 1</button>
+    <h2>{{ song.name }}</h2>
+    <p>{{ song.singer }}</p>
+    <audio controls :src="song.path"></audio>
+    <img :src="song.image" width="20" height="20" />
+  </div>
 </template>
+
+<script>
+import navar from '../components/layout/navar.vue';
+import foder from '../components/layout/foder.vue';
+import { mapActions, mapState } from 'vuex';
+export default {
+  components: {
+    navar,
+    foder,
+  },
+  computed: {
+    ...mapState('demo', ['songs', 'song']), // Thay 'demo' bằng tên namespace của Vuex store của bạn
+    filteredSongs() {
+      if (!this.searchQuery) {
+        return null;
+      }
+      const query = this.searchQuery.toLowerCase();
+      return this.songs.filter((item) => {
+        return item.name.toLowerCase().includes(query) || item.singer.toLowerCase().includes(query);
+      });
+    },
+  },
+  methods: {
+    ...mapActions('demo', ['setSong']), // Thay 'demo' bằng tên namespace của Vuex store của bạn
+    search() {
+      // Không cần gọi phương thức filter ở đây, computed property filteredSongs sẽ tự động cập nhật
+    },
+    setSongFiltered(song) {
+      this.setSong(song);
+    },
+  },
+};
+</script>
