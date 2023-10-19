c9dbcfbe2db57b78a3dc498d5e62cc1214b950cf
binhantt <doan44503@gmail.com>
2023-10-19 20:21:30 +0700

    them API

--- src/views/home.vue
+++ src/views/home.vue
@@ -2,13 +2,6 @@
   <div>
     <navar />
     <foder />
-    <div v-for="song in danhsach" :key="song.nhac.name">
-      {{ mame }};
-      <img :src="song.nhac.anh" :alt="song.nhac.name" />
-      <h3>{{ song.nhac.name }}</h3>
-      <p>{{ song.nhac.methor }}</p>
-      <audio :src="song.nhac.diachi" controls></audio>
-    </div>
   </div>
 </template>
 
@@ -22,9 +15,6 @@ export default {
     foder,
   },
   computed: {
-    danhsach() {
-      return this.$store.getters["demo/getAllSongs"];
-    },
   },
 };
 </script>
